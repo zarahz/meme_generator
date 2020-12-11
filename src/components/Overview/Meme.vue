e23a2b10b719b01d940837bedfef82bcc5c23000
Liliane Kabboura <kabboura@cip.ifi.lmu.de>
2020-12-11 18:09:35 +0100

    update the Meme.vue

--- src/components/Overview/Meme.vue
+++ src/components/Overview/Meme.vue
@@ -1,6 +1,6 @@
 <!-- File for the meme -> contains the meme header and image -->
 <template>
   <b-container class="justify-content-md-center" fluid>
-    <h2>Pick a template!</h2>
+    <h2>Meme View kommt später noch</h2>
     </b-container>
     </template>
