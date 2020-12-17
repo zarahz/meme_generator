38ecd0c8e155191bba43d8e566ed4f36ab2da268
Liliane Kabboura <lilianekabboura@hotmail.com>
2020-12-17 23:40:37 +0100

    code format

--- src/router/index.js
+++ src/router/index.js
@@ -9,36 +9,35 @@ import Login from "../components/Authenticate/Login.vue";
 import Registration from "../components/Authenticate/Registration.vue";
 
 
-   const routes = [
-      {
-        path: "/",
-        name: "Home",
-        component: CreateMeme
-      },
-      {
-        path: "/meme/:id",
-        name: "Meme",
-        props: true,
-        component: Meme
-      },
-      {
-        path: "/overviewpage",
-        name: "OverviewPage",
-        component: OverviewPage,
-        
-      },
-      {
-        path: "/login",
-        name: "Login",
-        component: Login,
-        
-      },
-      {
-        path: "/registration",
-        name: "Registration",
-        component:Registration,
-        
-      },
-    ];
-     export default new VueRouter ({mode: "history", routes: routes});
-  
+const routes = [
+  {
+    path: "/",
+    name: "Home",
+    component: CreateMeme
+  },
+  {
+    path: "/meme/:id",
+    name: "Meme",
+    props: true,
+    component: Meme
+  },
+  {
+    path: "/overviewpage",
+    name: "OverviewPage",
+    component: OverviewPage,
+
+  },
+  {
+    path: "/login",
+    name: "Login",
+    component: Login,
+
+  },
+  {
+    path: "/registration",
+    name: "Registration",
+    component: Registration,
+
+  },
+];
+export default new VueRouter({ mode: "history", routes: routes });
