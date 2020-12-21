5c1b9ffce90ca408e456e0357217be80660a2d53
Nsieyanji <Kevin.Nsieyanji@campus.lmu.de>
2020-12-21 11:11:15 +0100

    Revert changes on login vue

--- src/components/Authenticate/Login.vue
+++ src/components/Authenticate/Login.vue
@@ -1,6 +1,6 @@
 <template>
     <div class="vue-template">
-        <form class="login" @submit.prevent="login">
+        <form>
             <h3>Sign In</h3>
 
             <div class="form-group">
@@ -19,71 +19,22 @@
                 <router-link to="/forgot-password">Forgot password ?</router-link>
             </p>
 
-            <!--div class="social-icons">
+            <div class="social-icons">
                 <ul>
                     <li><a href="#"><i class="fa fa-google"></i></a></li>
                     <li><a href="#"><i class="fa fa-facebook"></i></a></li>
                     <li><a href="#"><i class="fa fa-twitter"></i></a></li>
                 </ul>
-            </div-->
+            </div>
 
         </form>
     </div>
 </template>
 
-
 <script>
-export default {
-  name: "Login",
-  data() {
-    return {
-      user: {
-        email: "",
-        password: "",
-      },
-      errors: {},
-    };
-  },
-  methods: {
-      
-    async checkUniqueness(fieldname, event) {
-      let result = await fetch("http://localhost:3000/is-unique", {
-        method: "POST",
-        headers: {
-          "Content-Type": "application/json",
-        },
-        body: JSON.stringify({ field: fieldname, value: event }),
-      });
-      if (result.status !== 200) {
-        const { error } = await result.json();
-        this.$set(this.errors, fieldname, error);
-      } else {
-        this.$delete(this.errors, fieldname);
-      }
-    },
-    async login(evt) {
-      evt.preventDefault();
-      if (Object.keys(this.errors).length === 0) {
-        //No problems, create new user!
-        console.log(this.user);
-        let result = await fetch("http://localhost:3000/login", {
-          method: "POST",
-          headers: {
-            "Content-Type": "application/json",
-          },
-          body: JSON.stringify(this.user),
-        });
-        if (result.status !== 200) {
-          const { error } = await result.json();
-          this.$set(this.errors, "login", error);
-        } else {
-          const { token } = await result.json();
-          console.log(token);
-          //TODO save token in local storage and get user by that!
-          this.$delete(this.errors, "login");
+    export default {
+        data() {
+            return {}
         }
-      }
-    },
-  },
-};
+    }
 </script>
